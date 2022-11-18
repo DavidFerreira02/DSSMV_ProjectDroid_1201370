8d576cbca122d01675f50e68381bde07923c524d
dspinda02 <davidbferreira02@gmail.com>
2022-11-18 10:54:04 +0000

    Project as it was before

---
+++ LibraryOnlineIsep/app/src/main/java/com/example/biblioteca/Helper/Utils.java
@@ -0,0 +1,52 @@
+package com.example.biblioteca.Helper;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class Utils {
+    private static final String WS_NAME = "v1/";
+    public static String isbn = "";
+    public static String userId = "";
+
+    public static final String BIBLIOTECAID = "1bbb3c13-e83f-4ea8-886e-5dd7c5feb187";
+
+    public static final String MYSHPREFS="MySharedPreferences";
+
+    public static final String IP1="IP1";
+    public static final String IP2="IP2";
+    public static final String IP3="IP3";
+    public static final String IP4="IP4";
+
+
+    public static String getWSAddress(Context context){
+        SharedPreferences settings = context.getSharedPreferences(MYSHPREFS, Context.MODE_PRIVATE);
+        int ip1 = settings.getInt(IP1, 193);
+        int ip2 = settings.getInt(IP2, 136);
+        int ip3 = settings.getInt(IP3, 62);
+        int ip4 = settings.getInt(IP4, 24);
+
+        return "http://"+ip1+"."+ip2+"."+ip3+"."+ip4+"/"+WS_NAME;
+    }
+    public static void setWSAddress(Context context, int ip1,int ip2,int ip3,int ip4 ){
+        SharedPreferences settings = context.getSharedPreferences(MYSHPREFS,Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putInt(IP1, ip1);
+        editor.putInt(IP2, ip2);
+        editor.putInt(IP3, ip3);
+        editor.putInt(IP4, ip4);
+        editor.commit();
+    }
+    /*public static int getIPNumber(Context context, String IP){
+        SharedPreferences settings = context.getSharedPreferences(MYSHPREFS, Context.MODE_PRIVATE);
+        return settings.getInt(IP, 0);
+    }
+    public static String getIPAddress(Context context){
+        SharedPreferences settings = context.getSharedPreferences(MYSHPREFS, Context.MODE_PRIVATE);
+        int ip1 = settings.getInt(IP1, 193);
+        int ip2 = settings.getInt(IP2, 136);
+        int ip3 = settings.getInt(IP3, 62);
+        int ip4 = settings.getInt(IP4, 24);
+
+        return ip1+"."+ip2+"."+ip3+"."+ip4;
+    }*/
+}
---
+++ LibraryOnlineIsep/app/src/main/java/com/example/biblioteca/UI/SettingsMain.java
@@ -0,0 +1,17 @@
+package com.example.biblioteca.UI;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.NumberPicker;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.biblioteca.Helper.Utils;
+import com.example.biblioteca.R;
+
+public class SettingsMain extends AppCompatActivity {
+
+
+}
